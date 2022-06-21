9f2581ceb3be62cc987bce61a5c13b234ad94559
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-21 11:12:02 +0600

    Time: 544 ms (5.02%), Space: 93.9 MB (5.10%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int n = heights.size();
+        
+        int l = 0, r = n-1, ans = 0;
+        
+        while(l <= r){
+            int m = (l + r)/2;
+            
+            if(okay(heights, m, bricks, ladders))
+                l = m + 1, ans = m;
+            else
+                r = m - 1;
+        }
+        
+        return ans;
+    }
+    
+    bool okay(vector<int> &h, int target, int bricks, int ladders){
+        vector<int> diffs;
+        for(int i = 1; i <= target; i++) {
+            if(h[i] > h[i-1])
+                diffs.emplace_back(h[i] - h[i-1]);
+        }        
+        sort(diffs.begin(), diffs.end(), greater<int>());
+        
+        int sum = 0;
+        for(int i = ladders; i < diffs.size(); i++)
+            sum += diffs[i];
+        
+        return sum <= bricks;
+    }
+};
