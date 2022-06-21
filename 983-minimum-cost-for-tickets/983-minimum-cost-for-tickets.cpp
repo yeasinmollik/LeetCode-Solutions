43a1749ada197e2e652b0aa2ae479e3cf9df01fe
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-21 21:17:13 +0600

    Time: 0 ms (100.00%), Space: 9.6 MB (71.32%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,21 @@
+//dp[i] = minimum cost to travel all days starting from days[i] to days[n-1]
+
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        vector<int> dp(n);
+        dp[n - 1] = min({costs[0], costs[1], costs[2]});
+        int intervals[3] = {1, 7, 30};
+        
+        for(int i = n - 2; i >= 0; i--) {
+            dp[i] = 1e9;
+            for(int j = 0; j < 3; j++) {
+                int next_idx = lower_bound(days.begin(), days.end(), days[i] + intervals[j]) - days.begin();
+                int next_cost = (next_idx == n ? 0 : dp[next_idx]);
+                dp[i] = min(dp[i], costs[j] + next_cost);                
+            }
+        } 
+        return dp[0];
+    }
+};
