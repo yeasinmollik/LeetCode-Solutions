a424d99336fa40ef548d4c86243cf8c8826a6681
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-21 23:49:22 +0600

    Time: 256 ms (81.29%), Space: 13.9 MB (88.50%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -8,7 +8,8 @@ public:
         short ans = 0;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++) {
-                dp[i][j] = (nums1[i-1] == nums2[j-1] ? 1 + dp[i-1][j-1] : 0);
+                if(nums1[i-1] == nums2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 if(ans < dp[i][j])
                     ans = dp[i][j];
             }
