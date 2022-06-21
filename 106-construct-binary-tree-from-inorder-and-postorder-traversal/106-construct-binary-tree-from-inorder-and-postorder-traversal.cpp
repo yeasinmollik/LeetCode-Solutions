7d1c3f66c49cac1759c336555345d62e167fc190
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-22 02:44:53 +0600

    Time: 21 ms (77.80%), Space: 26.5 MB (42.64%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int n, m;
-    unordered_map<int, int> pos;
+    unordered_map<int, int> index_of;
+    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        n = inorder.size(), m = postorder.size();
+        for(int i = 0; i < inorder.size(); i++)
+            index_of[inorder[i]] = i;
+        
         reverse(postorder.begin(), postorder.end());
-        for(int i = 0; i < n; i++)
-            pos[inorder[i]] = i;
-        return solve(inorder, 0, n-1, postorder, 0);
+        return getTree(inorder, 0, inorder.size() - 1, postorder, 0);
     }
     
-    TreeNode* solve(vector<int> &in, int l, int r, vector<int>& post, int id){
-        TreeNode* root = new TreeNode(post[id]);
-        int mid = pos[post[id]];
-        if(l <= mid-1)
-            root->left = solve(in, l, mid-1, post, id + r - mid + 1);
+    TreeNode* getTree(vector<int>& inorder, int l, int r, vector<int>& postorder, int id){
+        TreeNode* root = new TreeNode(postorder[id]);
+        int mid = index_of[postorder[id]];
+        
+        if(l <= mid - 1)
+            root->left = getTree(inorder, l, mid - 1, postorder, id + r - mid + 1);
+        
         if(mid + 1 <= r)
-            root->right = solve(in, mid + 1, r, post, id + 1);
+            root->right = getTree(inorder, mid + 1, r, postorder, id + 1);
+        
         return root;
     }
 };
