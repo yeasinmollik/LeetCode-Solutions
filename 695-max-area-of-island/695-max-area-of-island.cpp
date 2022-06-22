67a3e7c87a76ef28348e4e0ef6c7b5f9c68f1756
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-22 13:56:40 +0600

    Time: 28 ms (55.86%), Space: 23.3 MB (49.28%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -28,8 +28,12 @@ public:
         
         for(int k = 0; k < 4; k++){
             int p = i + x[k], q = j + y[k];
-            if(p >= 0 && p < n && q >= 0 && q < m && grid[p][q] && !vis[p][q])
+            if(valid(p, q) && grid[p][q] && !vis[p][q])
                 dfs(p, q, area, grid);
         }
+    }
+    
+    bool valid(int &i, int &j){
+        return i >= 0 && i < n && j >= 0 && j < m;
     }
 };
