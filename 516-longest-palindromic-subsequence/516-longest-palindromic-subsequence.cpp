57b7dccf8358de8e2f2a81bf0575661fd2487550
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-22 15:28:39 +0600

    Time: 76 ms (93.03%), Space: 10.9 MB (64.75%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -3,16 +3,15 @@ public:
     int dp[1000][1000];
     int longestPalindromeSubseq(string s) {
         memset(dp, -1, sizeof(dp));
-        return lps(0, s.size()-1, s);
+        
+        return lps(0, s.size() - 1, s);
     }
     
     int lps(int l, int r, string &s){
         if(l > r)
             return 0;
-        
         if(l == r)
             return 1;
-        
         if(dp[l][r] != -1)
             return dp[l][r];
         
