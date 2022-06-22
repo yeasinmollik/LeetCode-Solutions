2b57093a2f2888d4a9500ccb498ae77a6c82710b
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-22 14:12:45 +0600

    Time: 16 ms (83.23%), Space: 11.5 MB (57.70%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,16 +3,16 @@ public:
     int minDistance(string word1, string word2) {
        int n = word1.size(), m = word2.size();
         // space optimized lcs implementaion
-        int last[501] = {};
+        vector<int> last(m + 1);
         for(int i = 1; i <= n; i++){
-            int curr[501] = {};
+            vector<int> curr(m + 1);
             for(int j = 1; j <= m; j++){
                 if(word1[i-1] == word2[j-1])
                     curr[j] = 1 + last[j-1];
                 else
                     curr[j] = max(curr[j-1], last[j]);
             }
-            copy(begin(curr), end(curr), begin(last));
+            last = curr;
         }        
         return n + m - 2*last[m];
     }
