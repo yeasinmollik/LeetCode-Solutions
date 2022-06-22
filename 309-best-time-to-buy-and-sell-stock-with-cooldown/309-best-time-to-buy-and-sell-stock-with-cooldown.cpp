673a4ae239841b7370a37409c2acb657bf5c2887
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-23 02:03:05 +0600

    Time: 11 ms (31.80%), Space: 11.2 MB (70.43%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,17 +6,12 @@ public:
             return 0;
         
         int dp[5010] = {};
-        dp[0] = 0;
-        dp[1] = 0;
         dp[2] = (prices[1] > prices[0] ? prices[1] - prices[0] : 0);
+        int max_dp_minus_price = max(-prices[0], -prices[1]);
         
         for(int i = 3; i <= n; i++) {
-            int mx = max(dp[i-1], (prices[i-1] > prices[0] ? prices[i-1] - prices[0] : 0));
-            for(int j = 2; j < i; j++){
-                mx = max(mx, prices[i-1] - prices[j-1] + dp[j - 2]);
-            }
-            dp[i] = mx;
-            cout << dp[i] << " ";
+            dp[i] = max(dp[i-1], prices[i-1] + max_dp_minus_price);
+            max_dp_minus_price = max(max_dp_minus_price, dp[i - 2] - prices[i-1]);
         }
         
         return *max_element(dp, dp + 5010);
