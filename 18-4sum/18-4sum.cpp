10d6d4995cc9e1d2b9f3739c035a931b6fc4fb00
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-24 14:19:52 +0600

    Time: 67 ms (76.15%), Space: 13.2 MB (49.35%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        vector<vector<int>> ans;
+        for(int i = 0; i < n; i++){
+            if(i && nums[i-1] == nums[i])
+                continue;
+            auto res = threeSum(nums, i + 1, (long)target - nums[i]);
+            for(auto &x: res){
+                x.insert(x.begin(), nums[i]);
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+    
+    vector<vector<int>> threeSum(vector<int>& nums, int start, long tg) {
+        int n = nums.size();
+        
+        vector<vector<int>> ans;
+        for(int i = start; i < n; i++){
+            if(i > start && nums[i] == nums[i-1])
+                continue; 
+            
+            int l = i + 1, r = n - 1;
+            long target = tg - nums[i];
+            while(l < r) {
+                long sum = (long)nums[l] + nums[r];
+                if(sum == target){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    
+                    while(l < r && nums[l + 1] == nums[l])
+                        l++;
+                    while(l < r && nums[r - 1] == nums[r])
+                        r--;
+                    
+                    l++, r--;
+                }
+                else if(sum < target)
+                    l++;
+                else
+                    r--;
+            }
+        }
+        
+        return ans;
+    }
+};
