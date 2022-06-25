3511b70dddb2234d3a334861a582d060d5c17877
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-26 00:28:17 +0600

    Time: 200 ms (100.00%), Space: 8.7 MB (100.00%) - LeetHub

---
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // dp[i][j][k] = number of distinct sequences of length i, ending at j, k
+    int dp[10010][7][7];
+    int n;
+    int mod = 1e9 + 7;
+    
+    int distinctSequences(int x) {
+        n = x;
+        memset(dp, -1, sizeof(dp));
+        return dfs(0, 0, 0);
+    }
+    
+    int dfs(int i, int prev, int pre_prev){
+        if(i == n)
+            return 1;
+        
+        if(dp[i][prev][pre_prev] != -1)
+            return dp[i][prev][pre_prev];
+        
+        int total = 0;     
+        for(int curr = 1; curr < 7; curr++){
+            if(curr != prev && curr != pre_prev && (prev == 0 || __gcd(prev, curr) == 1))
+                total = (total + dfs(i + 1, curr, prev)) % mod;
+        }
+        
+        return dp[i][prev][pre_prev] = total;
+    }
+};
