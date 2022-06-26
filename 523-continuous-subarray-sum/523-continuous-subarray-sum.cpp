c579ded6312acd3902c908c651b1b94bb06de899
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-27 04:04:24 +0600

    Time: 269 ms (48.63%), Space: 96.6 MB (53.76%) - LeetHub

---
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> idx_of_first_mod;
+        
+        int sum = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            sum = (sum + nums[i-1])%k;
+            
+            if(sum == 0 && i >= 2)
+               return true;
+            
+            if(idx_of_first_mod[sum] && (i - idx_of_first_mod[sum]) >= 2)
+                return true;
+            
+            if(!idx_of_first_mod[sum])
+                idx_of_first_mod[sum] = i;
+        }       
+        return false;
+    }
+};
