7117cc03cd644af25c1c755b36b27f43766d2734
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-27 20:40:52 +0600

    Time: 653 ms (8.88%), Space: 89.7 MB (96.64%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -0,0 +1,27 @@
+//similar idea as Sliding Window Maximum
+
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        
+        deque<int> dq;
+        int max_value = INT_MIN;
+        
+        for(int r = 0; r < n; r++){
+            //removing out of range points
+            while(!dq.empty() && (points[r][0] - points[dq.front()][0]) > k)
+                dq.pop_front();
+            
+            if(!dq.empty())
+                max_value = max(max_value, points[r][0] + points[r][1] + points[dq.front()][1] - points[dq.front()][0]);
+            
+            // removing points having y - x values less than points[r]
+            while(!dq.empty() && points[dq.back()][1] - points[dq.back()][0] < points[r][1] - points[r][0])
+                dq.pop_back();
+            dq.push_back(r);
+        }
+        
+        return max_value;
+    }
+};
