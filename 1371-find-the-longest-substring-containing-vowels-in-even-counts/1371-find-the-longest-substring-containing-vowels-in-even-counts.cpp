0c3f08aa6ba50a6d0f805313f871e8e4e4481c89
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-28 15:48:27 +0600

    Time: 315 ms (9.36%), Space: 16.3 MB (31.29%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,28 @@
+#include <bitset>
+
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        int n = s.size();
+        bool vowel[26] = {};
+        vowel[0] = vowel[4] = vowel[8] = vowel[14] = vowel[20] = true;
+        
+        unordered_map<int, int> left_most;
+        int parity = 0;
+        left_most[parity] = 0;
+        int ans = 0;
+        
+        for(int i = 1; i <= n; i++){
+            if(vowel[s[i-1] - 'a'])
+                parity = parity ^ (1 << (s[i-1] - 'a'));
+            
+            if(left_most.count(parity))
+                ans = max(ans, i - left_most[parity]);
+            
+            if(!left_most.count(parity))
+                left_most[parity] = i;
+        }
+        
+        return ans;
+    }
+};
