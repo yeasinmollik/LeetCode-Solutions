45cdb99e6cd889acc8c5f5da55a800038ee68c26
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-29 23:42:17 +0600

    Time: 73 ms (55.28%), Space: 32.2 MB (48.29%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // helper function for serialize
+    void serializeHelper(TreeNode* root, string &s){
+        if(root == NULL){
+            s += "-9999 ";
+            return;
+        }
+        
+        s += to_string(root->val) + ' ';
+        serializeHelper(root->left, s);
+        serializeHelper(root->right, s);
+    }
+    
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string s;
+        serializeHelper(root, s);
+        return s;
+    }
+    
+    
+    // split string to integer vector
+    vector<int> split(string s){
+        vector<int> splited;
+        istringstream iss(s);
+        string w;
+        
+        while(getline(iss, w, ' ')){
+            if(!w.empty())
+                splited.push_back(stoi(w));
+        }
+        return splited;
+    }
+    
+    TreeNode* deserializeHelper(vector<int> &vals, int &idx){
+        if(vals[idx] == -9999) {
+            idx++;
+            return NULL;
+        }
+        
+        TreeNode* root = new TreeNode(vals[idx++]);
+        root->left = deserializeHelper(vals, idx);
+        root->right = deserializeHelper(vals, idx);
+        
+        return root;
+    }
+    
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        vector<int> vals = split(data);
+        int idx = 0;
+        return deserializeHelper(vals, idx);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
