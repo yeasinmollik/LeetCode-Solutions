3257b9946e346dcac928d39d8c14d919ddec8b7f
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-30 11:25:53 +0600

    Time: 48 ms (79.84%), Space: 28.4 MB (41.83%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int ans = 0;   
+        for(int &x: nums)
+            ans += x - mn;
+        
+        return ans;
+    }
+};
