c6f429de6e34ce64d3ed676b97d2fd50222ec55d
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-30 13:00:27 +0600

    Time: 14 ms (69.51%), Space: 11.3 MB (5.28%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -8,11 +8,9 @@ public:
         for(int i = 1, j = n; i <= n; i++, j--){
             p[i] = p[i-1] + nums[i-1];
             s[j] = s[j + 1] + nums[j-1];
-            //cout << p[i] << " " << s[j] << endl;
         }
         
         long ans = min(s[1] - nums[0]*long(n - 1), nums[n-1]*long(n-1)- p[n-1]);
-        //cout << ans << endl;
         for(int i = 2; i < n; i++) {
             long left = long(i-1) * nums[i-1] - p[i-1];
             long right = s[i + 1] - long(n - i)*nums[i-1];
