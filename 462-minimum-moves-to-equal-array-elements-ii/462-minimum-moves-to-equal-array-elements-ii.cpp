9e537315e1100f6d8df8bebb5bb76901c13ef573
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-06-30 12:57:39 +0600

    Time: 15 ms (63.23%), Space: 11.8 MB (5.28%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        vector<long> p(n + 1), s(n + 2);
+        for(int i = 1, j = n; i <= n; i++, j--){
+            p[i] = p[i-1] + nums[i-1];
+            s[j] = s[j + 1] + nums[j-1];
+            //cout << p[i] << " " << s[j] << endl;
+        }
+        
+        long ans = min(s[1] - nums[0]*long(n - 1), nums[n-1]*long(n-1)- p[n-1]);
+        //cout << ans << endl;
+        for(int i = 2; i < n; i++) {
+            long left = long(i-1) * nums[i-1] - p[i-1];
+            long right = s[i + 1] - long(n - i)*nums[i-1];
+            ans = min(ans, left + right);
+        }
+        return ans;
+    }
+};
