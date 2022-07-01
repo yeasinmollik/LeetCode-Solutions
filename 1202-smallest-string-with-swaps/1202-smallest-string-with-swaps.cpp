0f2ccada4840016f7ada363ce4e2917d7b8261cb
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-01 14:46:37 +0600

    Time: 380 ms (25.34%), Space: 67.4 MB (27.10%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> adj;
+    bool vis[100010] = {};
+    
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int n = s.size(), m = pairs.size();
+        adj.resize(n);
+        
+        for(auto &p: pairs)
+            adj[p[0]].push_back(p[1]), adj[p[1]].push_back(p[0]);
+        
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]){
+                vector<int> connected;
+                string t;
+                dfs(i, connected, s, t);
+                
+                sort(t.begin(), t.end());
+                sort(connected.begin(), connected.end());
+                for(int j = 0; j < connected.size(); j++)
+                    s[connected[j]] = t[j];
+            }
+        }
+        return s;
+    }
+    
+    void dfs(int u, vector<int>& connected, string &s, string& t){
+        vis[u] = true;
+        connected.push_back(u);
+        t.push_back(s[u]);
+        
+        for(int &v: adj[u])
+            if(!vis[v])
+                dfs(v, connected, s, t);
+    }
+};
