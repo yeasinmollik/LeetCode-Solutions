aee01ade37e749f2a609eb9ed3ef21206b1a4559
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-05 05:33:16 +0600

    Time: 169 ms (5.06%), Space: 28.8 MB (6.83%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int candy(vector<int>& r) {
-        // vector<int> pos[10010];
+        vector<int> pos[20010];
         int n = r.size();
         r.insert(r.begin(), 1e9);
         r.push_back(1e9);
         
-        map<int, vector<int>> pos;
+        set<int> s;
         for(int i = 1; i <= n; i++){
             pos[r[i]].emplace_back(i);
+            s.insert(r[i]);
         }
         
         // for(auto &x: pos) {
@@ -20,11 +21,11 @@ public:
         int ans = 0;
         int candies[20010] = {};
         
-        for(int x: pos.begin()->second)
+        for(int x: pos[*s.begin()])
             candies[x] = 1, ans++;
         
-        for(auto it = next(pos.begin()); it != pos.end(); it++){
-            for(int x: it->second){
+        for(auto it = next(s.begin()); it != s.end(); it++){
+            for(int x: pos[*it]){
                 if(r[x-1] >= r[x] && r[x + 1] >= r[x]) {
                     candies[x] = 1;
                     ans++;
