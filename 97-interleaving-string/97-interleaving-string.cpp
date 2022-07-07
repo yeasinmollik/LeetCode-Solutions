0a5d8d1adc12f007d10e078cad7dd81c9fe5f3c5
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-07 21:06:26 +0600

    Time: 61 ms (19.97%), Space: 10.8 MB (38.77%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -5,43 +5,37 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size())
             return false;
-
-        memset(dp, -1, sizeof(dp)); 
-        return solve(s1, s2, s3, 0, 0, 0, true) || solve(s1, s2, s3, 0, 0, 0, false);
+        
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, 0, s1, s2, s3, true) | solve(0, 0, 0, s1, s2, s3, false);
     }
     
-    bool solve(string &s1, string &s2, string &s3, int i, int j, int k, bool one){
-        if(k == s3.size())
+    bool solve(int i, int j, int k, string &s1, string &s2, string &s3, bool first){
+        if(k == s3.size())  //base case
             return true;
         
-        if(dp[i][j][k][(int)one] != -1)
-            return dp[i][j][k][(int)one];
+        if(dp[i][j][k][first] != -1)
+            return dp[i][j][k][first];
         
-        if(one){
-            if(s1[i] != s3[k])
-                return false;
-            
-            bool solved = false;
+        bool solved = false;
+        
+        if(first){
             for(int i1 = i, k1 = k; i1 < s1.size() && !solved; i1++, k1++){
                 if(s1[i1] == s3[k1])
-                    solved = solve(s1, s2, s3, i1 + 1, j, k1 + 1, !one);
+                    solved = solve(i1 + 1, j, k1 + 1, s1, s2, s3, !first);
                 else
                     break;
-            }
-            return dp[i][j][k][(int)one] = solved;
+            }            
         }
-        else {
-            if(s2[j] != s3[k])
-                return false;
-            
-            bool solved = false;
+        else{
             for(int j1 = j, k1 = k; j1 < s2.size() && !solved; j1++, k1++){
                 if(s2[j1] == s3[k1])
-                    solved = solve(s1, s2, s3, i, j1 + 1, k1 + 1, !one);
+                    solved = solve(i, j1 + 1, k1 + 1, s1, s2, s3, !first);
                 else
                     break;
             }
-            return dp[i][j][k][(int)one] = solved;
         }
+        
+        return dp[i][j][k][first] = solved;
     }
 };
