ea0285a1af07a4b70605cf13372f9d26195104f4
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-08 06:37:10 +0600

    Time: 700 ms (51.63%), Space: 89.7 MB (68.10%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -15,8 +15,7 @@ public:
         // dummy point to make the code concise and clean
         points.push_back({INT_MAX, INT_MIN});  
         int shots = 0;
-        auto prev = points.front();
-        
+        auto prev = points.front(); 
         for(int i = 1; i <= n; i++){
             if(dontOverlap(prev, points[i])) 
                 shots++, prev = points[i];
