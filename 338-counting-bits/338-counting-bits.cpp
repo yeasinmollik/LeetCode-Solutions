f37d99c1a08ffa9adf14bd1a83ab2f81a040a3a5
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-12 06:19:24 +0600

    Time: 18 ms (7.88%), Space: 8.6 MB (21.06%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        
+        for(int i = 0; i <= n; i++){
+            int num = i, cnt = 0;
+            while(num){
+                cnt += num & 1;
+                num /= 2;
+            }
+            ans.emplace_back(cnt);
+        }
+        return ans;
+    }
+};
