7211639fd85b79c33c57bfe9f71cad0d65828797
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-14 03:38:45 +0600

    Time: 13 ms (10.87%), Space: 13.4 MB (9.87%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if(root)
-            dfs(root, 0, ans);
+        dfs(root, 0, ans);
         return ans;
     }
     
     void dfs(TreeNode *root, int level, vector<vector<int>> &ans){
+        if(!root)
+            return;
+        
         if(level >= ans.size())
             ans.push_back({});
         
         ans[level].emplace_back(root->val);
-        if(root->left)
-            dfs(root->left, level + 1, ans);
-        if(root->right)
-            dfs(root->right, level + 1, ans);
+        dfs(root->left, level + 1, ans);
+        dfs(root->right, level + 1, ans);
     }
 };
