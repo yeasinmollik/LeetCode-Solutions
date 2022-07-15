1903334bd30ff8b84a1d5460e161d31afd39c264
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-16 00:39:41 +0600

    Time: 35 ms (38.59%), Space: 23.3 MB (48.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     int n, m;
-    int x[4] = {1, 0, 0, -1}, y[4] = {0, 1, -1, 0};
-    bool vis[50][50] = {};
+    bool vis[51][51] = {};
+    int x[4] = {1, 0, -1, 0}, y[4] = {0, 1, 0, -1};
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         n = grid.size(), m = grid[0].size();
         
-        int maxArea = 0;
+        int mxArea = 0;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] && !vis[i][j]){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     int area = 0;
                     dfs(i, j, area, grid);
-                    if(area > maxArea)
-                        maxArea = area;
+                    mxArea = max(mxArea, area);
                 }
             }
-        }  
-        return maxArea;
+        }
+        
+        return mxArea;
     }
     
-    void dfs(int i, int j, int &area, vector<vector<int>> &grid){
+    void dfs(int i, int j, int &area, vector<vector<int>>& grid){
         area++;
         vis[i][j] = true;
         
         for(int k = 0; k < 4; k++){
-            int p = i + x[k], q = j + y[k];
-            if(valid(p, q) && grid[p][q] && !vis[p][q])
-                dfs(p, q, area, grid);
+            int i1 = i + x[k], j1 = j + y[k];
+            if(valid(i1, j1) && grid[i1][j1] == 1 && !vis[i1][j1])
+                dfs(i1, j1, area, grid);
         }
     }
     
-    bool valid(int &i, int &j){
+    bool valid(int i, int j){
         return i >= 0 && i < n && j >= 0 && j < m;
     }
 };
