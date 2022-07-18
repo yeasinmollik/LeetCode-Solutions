00603501e8171998ca875372cee745f3b4593f22
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-18 06:45:44 +0600

    Time: 1370 ms (22.65%), Space: 9.4 MB (67.73%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = 0;
-        sum = accumulate(nums.begin(), nums.end(), sum);
-        if (nums.size() < k || sum % k) return false;
-        
-        vector<bool> visited(nums.size(), false);
-        int univisited = nums.size();
-        return backtrack(nums, visited, sum / k, 0, 0, k, univisited);
-    }
     
-    bool backtrack(vector<int>& nums, vector<bool> &visited, int target, int curr_sum, int i, int k, int &unvisited) {
-        if (k == 1) 
-            return true;
-        
-        if(unvisited == 0 || i >= nums.size())
-            return false;
-        
-        if (curr_sum == target) 
-            return backtrack(nums, visited, target, 0, 0, k-1, unvisited);
-        
-        for (int j = i; j < nums.size(); j++) {
-            if (visited[j] || curr_sum + nums[j] > target) continue;
-            
-            visited[j] = true;
-            unvisited--;
-            if (backtrack(nums, visited, target, curr_sum + nums[j], j+1, k, unvisited)) return true;
-            visited[j] = false;
-            unvisited++;
+      int sum =accumulate(nums.begin(),nums.end(),0);
+      vector<bool>vis(nums.size(),false);
+      if(sum%k!=0)
+        return false;
+      int s=sum/k;
+      sort(begin(nums),end(nums),greater<int>());// For avoid extra calculation
+      return is_possible(nums,0,s,k,0,vis);
+  }
+     bool is_possible(vector<int>&nums,int curr,int sum,int k,int start,vector<bool>&vis)
+    {
+            if(k==1)
+                return true;
+            if(start>=nums.size()) //This line is important to avoid tle
+               return false;
+            if(curr==sum)
+                    return is_possible(nums,0,sum,k-1,0,vis);
+
+        for(int i=start;i<nums.size();i++)
+        {
+            if(vis[i] || nums[i]+curr>sum)
+                continue;
+            vis[i]=true;
+            if(is_possible(nums,curr+nums[i],sum,k,i+1,vis))
+                return true;
+            vis[i]=false;
         }
-        
         return false;
     }
 };
