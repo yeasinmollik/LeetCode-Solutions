655fcfd7f4906134d9438a652b9b29918515905b
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-07-19 11:33:35 +0600

    Time: 1236 ms (26.34%), Space: 9.4 MB (67.73%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-   // use global variables to avoid long parameter list
-   int target; // of each bucket
-   vector< int > ns;
-   vector< int > bucket;
-
-   bool canPartitionKSubsets( vector<int>& nums, int k ) {
-       int sum = 0;
-       for( int &n : nums ) 
-           sum += n;
-       
-       if( sum % k ) 
-           return false; // not divisible
-       
-       target = sum / k;
-       ns = vector< int >( nums );
-       bucket = vector< int >( k, 0 );
-       
-       // starting with bigger ones makes it faster
-       sort( ns.begin(), ns.end() );
-       reverse( ns.begin(), ns.end() );
-       
-       return put( 0 );
-   }
-
-   // put #n item of ns into some bucket to meet target
-   bool put( int n ) {
-       if(n == ns.size())
-           return true;
-       
-       for( int i = 0; i < bucket.size(); ++i ) {
-           if( bucket[i] + ns[n] > target ) 
-               continue; // try next bucket
-           
-           bucket[i] += ns[n]; // put it in!
-           
-           if( put( n + 1 ) ) 
-               return true; // move on to next item
-           
-           bucket[i] -= ns[n]; // take it out
-           if(bucket[i] == 0)
-                   return false;
-       }
-       return false; // no bucket fits
-   }
+    int n;
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        n = nums.size();
+        if(n < k)
+            return false;
+        
+        int tot = 0;
+        for(int &x: nums)
+            tot += x;
+        
+        if(tot % k != 0)
+            return false;
+     
+        vector<int> buckets(k, 0);
+        
+        return solve(nums, buckets, k, tot / k, 0);
+    }
+    
+    bool solve(vector<int> &nums, vector<int> &buckets, int k, int target, int idx){
+        if(idx == n)
+            return true;
+        
+        for(int i = 0; i < k; i++){
+            if(buckets[i] + nums[idx] > target)
+                continue;
+            
+            buckets[i] += nums[idx];
+            
+            if(solve(nums, buckets, k, target, idx + 1) == true)
+                return true;
+            
+            buckets[i] -= nums[idx];
+            if(buckets[i] == 0)
+                return false;
+        }
+        return false;
+    }
 };
