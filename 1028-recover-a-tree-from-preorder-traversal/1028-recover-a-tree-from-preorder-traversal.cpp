204ed56918142c2db883e5fbe9424a83942ee466
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-22 17:17:45 +0600

    Time: 27 ms (69.29%), Space: 20.7 MB (29.67%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        vector<int> pre;
+        int num = 0, cnt = 0;
+        vector<int> vals, levels = {0};
+        
+        for(int i = 0; i < traversal.size(); i++){
+            if(traversal[i] == '-') {
+                if(num > 0)
+                    vals.push_back(num), num = 0;
+                cnt++;
+            }
+            else{
+                if(cnt > 0)
+                    levels.push_back(cnt), cnt = 0;
+                num = num * 10 + traversal[i] - '0';                
+            }
+        }
+        vals.push_back(num);
+        
+        int lid = 0, vid = 0;
+        return build(vals, levels, lid, vid, 0);
+    }
+    
+    TreeNode* build(vector<int> &vals, vector<int> &levels, int &lid, int &vid, int level){
+        if(lid == levels.size() || levels[lid] != level)
+            return NULL;
+        
+        TreeNode *root = new TreeNode(vals[vid]);
+        vid++, lid++;
+        root->left = build(vals, levels, lid, vid, level + 1);
+        root->right = build(vals, levels, lid, vid, level + 1);
+        return root;
+    }
+};
