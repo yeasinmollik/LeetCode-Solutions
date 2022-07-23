43c1b2589cb8af96ca4ff9535b985c4cd0ce8907
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-23 23:29:34 +0600

    Time: 0 ms (100.00%), Space: 7 MB (99.50%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int m = 2*n;
+        int mx = 1<<m;
+        vector<string> ans;
+        
+        for(int i = 0; i < mx; i++){
+            if(__builtin_popcount(i) != n)
+                continue;
+            
+            int sum = 0, valid = 1; 
+            for(int j = 0; j < m; j++){
+                sum += (i & (1 << j))? 1 : -1;
+                if(sum < 0){
+                    valid = 0;
+                    break;
+                }
+            }
+            
+            if(valid)
+                ans.emplace_back(buildParenthesis(i, m));
+        }
+        
+        return ans;
+    }
+    
+    string buildParenthesis(int &x, int &m){
+        string s;
+        for(int i = 0; i < m; i++){
+            if(x & (1 << i))
+                s.push_back('(');
+            else
+                s.push_back(')');
+        }
+        return s;
+    }
+};
