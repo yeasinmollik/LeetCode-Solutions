30e3504786acda429d66733660f42093407a3a0b
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-23 21:25:47 +0600

    Time: 484 ms (36.64%), Space: 8.4 MB (8.74%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int x[4] = {1, 0, 0, -1}, y[4] = {0, 1, -1, 0};
+    int n, m, mx = 0;
+    
+    int getMaximumGold(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j]){
+                    vector<vector<bool>> vis(n, vector<bool>(m));
+                    vis[i][j] = true;
+                    dfs(i, j, grid[i][j], vis, grid);
+                }
+            }
+        }
+        
+        return mx;
+    }
+    
+    void dfs(int i, int j, int gold, vector<vector<bool>> &vis, vector<vector<int>>& grid){
+        mx = max(mx, gold);
+        for(int k = 0; k < 4; k++){
+            int i1 = i + x[k], j1 = j + y[k];
+            if(valid(i1, j1) && grid[i1][j1] && !vis[i1][j1]){
+                vis[i1][j1] = true;
+                dfs(i1, j1, gold + grid[i1][j1], vis, grid);
+                vis[i1][j1] = false;
+            }
+        }
+    }
+    
+    bool valid(int i, int j){
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+};
