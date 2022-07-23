a25ea335ac6bd798ba90ea8b17c0c43fc5993f54
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-23 21:27:39 +0600

    Time: 297 ms (85.56%), Space: 7.6 MB (42.83%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int x[4] = {1, 0, 0, -1}, y[4] = {0, 1, -1, 0};
     int n, m, mx = 0;
-    
+    int vis[110][110];
+                 
     int getMaximumGold(vector<vector<int>>& grid) {
         n = grid.size(), m = grid[0].size();
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j]){
-                    vector<vector<bool>> vis(n, vector<bool>(m));
+                    memset(vis, 0, sizeof(vis));
                     vis[i][j] = true;
-                    dfs(i, j, grid[i][j], vis, grid);
+                    dfs(i, j, grid[i][j], grid);
                 }
             }
         }
@@ -19,13 +20,13 @@ public:
         return mx;
     }
     
-    void dfs(int i, int j, int gold, vector<vector<bool>> &vis, vector<vector<int>>& grid){
+    void dfs(int i, int j, int gold, vector<vector<int>>& grid){
         mx = max(mx, gold);
         for(int k = 0; k < 4; k++){
             int i1 = i + x[k], j1 = j + y[k];
             if(valid(i1, j1) && grid[i1][j1] && !vis[i1][j1]){
                 vis[i1][j1] = true;
-                dfs(i1, j1, gold + grid[i1][j1], vis, grid);
+                dfs(i1, j1, gold + grid[i1][j1], grid);
                 vis[i1][j1] = false;
             }
         }
