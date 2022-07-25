6d07ddc897f0570e8293b94bf52bc59d03259356
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-25 23:28:04 +0600

    Time: 4 ms (40.34%), Space: 6.7 MB (28.04%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -3,20 +3,20 @@ public:
     string reverseParentheses(string s) {
         stack<string> st;
         
-        string curr = "";
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(curr);
-                curr = "";
+        string t = "";
+        for(char &c: s){
+            if(c == '('){
+                st.push(t);
+                t = "";
             }
-            else if(s[i] == ')'){
-                reverse(curr.begin(), curr.end());
-                curr = st.top() + curr;
+            else if(c == ')'){
+                reverse(t.begin(), t.end());
+                t = st.top() + t;
                 st.pop();
             }
             else
-                curr.push_back(s[i]);
+                t.push_back(c);
         }
-        return curr;
+        return t;
     }
 };
