59f006b426a7112d365a2599688eff5545aa8d52
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-26 19:05:19 +0600

    Time: 534 ms (42.98%), Space: 122.2 MB (39.38%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -5,17 +5,20 @@ public:
         vector<int> dp(n);
         
         deque<int> dq;
+        int maxSubsetSum = INT_MIN;
         for(int i = 0; i < n; i++){
             while(!dq.empty() && dq.front() < (i - k))
                 dq.pop_front();
             
             dp[i] = max(nums[i], !dq.empty()? dp[dq.front()] + nums[i]: INT_MIN);
+            if(dp[i] > maxSubsetSum)
+                maxSubsetSum = dp[i];
             
             while(!dq.empty() && dp[dq.back()] < dp[i])
                 dq.pop_back();
             dq.push_back(i);                
         }
-        return *max_element(dp.begin(), dp.end());
+        return maxSubsetSum;
     }
       
 };
