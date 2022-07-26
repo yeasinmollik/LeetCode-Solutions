d5f62c74e6352cac1e76e8fb10ef99ba8a26bd07
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-26 23:14:47 +0600

    Time: 372 ms (54.23%), Space: 112.8 MB (13.77%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, unordered_set<int>> m;
+        for(int i = 0; i < arr.size(); i++)
+            m[arr[i]].insert(i);
+        
+        queue<pair<int, int>> q; // <index, jumps>
+        q.push({0, 0});
+        bool vis[50010] = {};
+        vis[0] = true;
+        m[arr[0]].erase(0);
+        
+        while(!q.empty()){
+            auto[idx, jumps] = q.front();
+            q.pop();
+            
+            if(idx == n - 1)
+                return jumps;
+            
+            for(auto it = m[arr[idx]].begin(); it != m[arr[idx]].end();){
+                q.push({*it, jumps + 1});
+                vis[*it] = true;
+                it = m[arr[idx]].erase(it);
+            }
+            
+            int prev = idx - 1, next = idx + 1;
+            if(prev >= 0 && !vis[prev])
+                q.push({prev, jumps + 1}), vis[prev] = true, m[arr[prev]].erase(prev);
+            
+            if(next < n && !vis[next])
+                q.push({next, jumps + 1}), vis[next] = true, m[arr[next]].erase(next);
+        }
+        return false;
+    }
+};
