820458383f30e21a9534f8584d6079e390f7872a
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-26 21:43:00 +0600

    Time: 90 ms (27.51%), Space: 31.3 MB (77.53%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        bool vis[50010] = {};
+        
+        queue<int> q;
+        q.push(start);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            if(arr[curr] == 0)
+                return true;
+            
+            int left = curr - arr[curr];
+            int right = curr + arr[curr];
+            
+            if(left >= 0 && !vis[left])
+                q.push(left), vis[left] = true;
+            
+            if(right < n && !vis[right])
+                q.push(right), vis[right] = true;
+        }
+        return false;
+    }
+};
