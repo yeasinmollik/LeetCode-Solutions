939234e178ca29a77bfe3c0adcccb1588cc81ab4
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-07-28 12:03:23 +0600

    Time: 11 ms (70.38%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int cnt[26] = {};
+        
+        for(char &c: t)
+            cnt[c - 'a']++;
+        
+        for(char &c: s)
+            cnt[c - 'a']--;
+        
+        for(int i = 0; i < 26; i++)
+            if(cnt[i])
+                return false;
+        return true;
+    }
+};
