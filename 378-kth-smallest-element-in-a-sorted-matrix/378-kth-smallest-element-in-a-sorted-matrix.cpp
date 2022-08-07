3ac2e81da30855fc8df5b649f357aba9bd41af67
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-07 18:20:13 +0600

    Time: 75 ms (22.13%), Space: 13.2 MB (59.46%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,22 +2,21 @@ typedef tuple<int, int, int> tupl;
 
 class Solution {
 public:
-    int n, m, inf = 1e9 + 7;
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        n = matrix.size(), m = matrix[0].size();
-        int cnt = 0, ans;
-        
+        int n = matrix.size();
         priority_queue<tupl, vector<tupl>, greater<tupl>> pq;
-        for(int i = 0; i < m && i < k; i++){
+        for(int i = 0; i < n; i++)
             pq.push({matrix[0][i], 0, i});
-        }
-        for(int l = 0; l < k; l++){
-            auto[val, i, j] = pq.top();
+        
+        int ans;
+        for(int i = 0; i < k; i++){
+            auto [val, x, y] = pq.top();
             pq.pop();
             ans = val;
-            if(i + 1 < n)
-                pq.push({matrix[i+1][j], i + 1, j});
+            if(x + 1 < n)
+                pq.push({matrix[x + 1][y], x + 1, y});
         }
         return ans;
     }
 };
+    
