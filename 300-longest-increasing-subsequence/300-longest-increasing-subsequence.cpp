3dcedabdda8a02e2c28e40280514b671ef557a6d
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-08 18:39:59 +0600

    Time: 16 ms (81.01%), Space: 10.3 MB (97.96%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
+    int zero = 1e4 + 1;
+    int mx = 2 * zero + 1;
+    int bt[20010] = {};
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int dp[2510] = {}; 
-        dp[0] = 1;
-        int ans = 1;
-        for(int i = 1; i < n; i++){
-            dp[i] = 1;
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i])
-                    dp[i] = max(dp[i], dp[j] + 1);
-            }
-            ans = max(ans, dp[i]);
+        int lis = 1;
+        for(int i = 0; i < n; i++){
+            int lis_here = query(nums[i] + zero - 1) + 1;
+            if(lis_here > lis)
+                lis = lis_here;
+            update(nums[i] + zero, lis_here);
         }
-        return ans;
+        
+        return lis;
+    }
+    
+    void update(int idx, int val){
+        while(idx < mx){
+            bt[idx] = max(bt[idx], val);
+            idx += idx & (-idx);
+        }
+    }
+    
+    int query(int idx){
+        int mx = 0;
+        while(idx > 0){
+            mx = max(mx, bt[idx]);
+            idx -= idx & (-idx);
+        }
+        return mx;
     }
 };
