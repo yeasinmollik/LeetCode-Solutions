ad02559d0ee4a29eae098a69c39d6bd3aba5a0d7
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-12 05:58:04 +0600

    Time: 31 ms (47.20%), Space: 7.3 MB (95.07%) - LeetHub

--- 813-largest-sum-of-averages/813-largest-sum-of-averages.cpp
+++ 813-largest-sum-of-averages/813-largest-sum-of-averages.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int n;
-    double ninf = -1e9, eps = 1e-9;
-    map<pair<int, int>, double> dp;
+    double dp[101][101];
+    bool done[101][101] = {};
+    double ninf = -1e9, eps = 1e-6;
     
     double largestSumOfAverages(vector<int>& nums, int k) {
         n = nums.size();
@@ -16,14 +17,16 @@ public:
         if(k <= 0)
             return ninf;
         
-        if(dp.find({idx, k}) != dp.end())
-            return dp[{idx,k}];
+        if(done[idx][k])
+            return dp[idx][k];
         
         double sum = 0, avg = ninf;
         for(int i = idx; i < n; i++){
             sum += nums[i];
             avg = max(avg, sum / (i - idx + 1) + getMaxAvg(i + 1, k-1, nums));
         }
-        return dp[{idx,k}] = avg;
+        
+        done[idx][k] = true;
+        return dp[idx][k] = avg;
     }
 };
