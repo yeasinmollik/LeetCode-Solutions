d21291741351a51145f341fb0f824a8bfb5595dd
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-12 16:37:25 +0600

    Time: 10 ms (16.99%), Space: 8.1 MB (68.84%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        
+        unordered_map<int, int> cnt, ccnt;
+        for(int i = 0; i < n; i++){
+            cnt[arr[i]]++;    
+        }
+        
+        for(auto x: cnt){
+            if(ccnt[x.second])
+                return false;
+            ccnt[x.second]++;
+        }
+        return true;
+    }
+};
