92e7cda9d529daf5c58b7269f1c79cbe4db49aa9
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-18 09:18:18 +0600

    Time: 206 ms (94.14%), Space: 55.4 MB (99.84%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int cnt[100010] = {};
+        for(int x: arr)
+            cnt[x]++;
+        
+        sort(cnt, cnt + 100010, greater<int>());
+        int to_remove = arr.size()/2;
+        for(int i = 0; i < 100010; i++){
+            to_remove -= cnt[i];
+            if(to_remove <= 0)
+                return i + 1;
+        }
+        
+        return 0;
+    }
+};
