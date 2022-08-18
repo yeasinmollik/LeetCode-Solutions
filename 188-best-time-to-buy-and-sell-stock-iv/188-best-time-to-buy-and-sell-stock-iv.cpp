c02dab06ce869aa2313b0705f7932dc3a9d73c1f
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-18 08:18:13 +0600

    Time: 929 ms (6.03%), Space: 11.3 MB (71.13%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    int dp[1010][110];
+    
+    int maxProfit(int k, vector<int>& prices) {
+        n = prices.size();    
+        memset(dp, -1, sizeof(dp));
+        return solve(0, k, prices);
+    }
+    
+    int solve(int curr, int k, vector<int> &prices){        
+        if(curr == n || k < 1)
+            return 0;
+        
+        if(dp[curr][k] != -1)
+            return dp[curr][k];
+        
+        int mx = solve(curr + 1, k, prices);
+        for(int i = curr + 1; i < n; i++){
+            if(prices[i] >= prices[curr])            
+                mx = max(mx, prices[i] - prices[curr] + solve(i + 1, k - 1, prices));
+        }
+        
+        return dp[curr][k] = mx;
+    }
+};
