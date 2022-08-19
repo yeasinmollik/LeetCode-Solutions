38f8ea29f15e51feca20a478758a59b7204331f6
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-19 15:16:45 +0600

    Time: 24 ms (36.13%), Space: 5.8 MB (69.73%) - LeetHub

---
+++ 668-kth-smallest-number-in-multiplication-table/668-kth-smallest-number-in-multiplication-table.cpp
@@ -0,0 +1,27 @@
+typedef tuple<int, int, int> tupl;
+
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        int ans = m * n;
+        
+        while(left <= right){
+            int mid  = (left + right)/2;
+            int cnt = count(m, n, mid);
+            if(cnt >= k)
+                right = mid - 1, ans = mid;
+            else 
+                left = mid + 1;
+        }
+        return ans;
+    }
+    
+    int count(int m, int n, int num){
+        int cnt = 0;
+        for(int i = 1; i <= m; i++){
+            cnt += min(n, num / i);
+        }
+        return cnt;
+    }
+};
