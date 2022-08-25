19081cd713e82476793cec9c85d8bd3d40d58563
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-25 06:03:46 +0600

    Time: 8 ms (97.81%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int cnt[26] = {};
+        for(char c: magazine)
+            cnt[c-'a']++;
+        for(char c: ransomNote){
+            cnt[c-'a']--;
+            if(cnt[c-'a'] < 0)
+                return false;
+        }
+        return true;
+    }
+};
