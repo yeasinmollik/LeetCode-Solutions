a6256b6a8548dcba40678f0768011707ae6d3d18
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-08-26 06:10:58 +0600

    Time: 116 ms (10.17%), Space: 5.9 MB (96.61%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        vector<long> pw;
-        for(int i = 0; i <= 32; i++)
-            pw.push_back((long)1<<i);
+        int pw[31];
+        for(int i = 0; i <= 30; i++)
+            pw[i] = (1<<i);
         
         string s = to_string(n);
         sort(s.begin(), s.end());
         do{
-            if(s.front() != '0' && binary_search(pw.begin(), pw.end(), stol(s)))
+            if(s.front() != '0' && binary_search(begin(pw), end(pw), stol(s)))
                 return true;
         }while(next_permutation(s.begin(), s.end()));
         return false;
