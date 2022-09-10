dbda5ca0ef1a6fe38f8f30d35a97fcdb88ee2c0a
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-09-10 21:04:53 +0600

    Time: 1521 ms (5.01%), Space: 11.3 MB (72.11%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int n;
     int dp[1010][110];
+    int n;
     
     int maxProfit(int k, vector<int>& prices) {
-        n = prices.size();    
-        
+        n = prices.size();
         memset(dp, -1, sizeof(dp));
         return solve(0, k, prices);
     }
     
-    int solve(int curr, int k, vector<int> &prices){        
-        if(curr == n || k < 1)
+    int solve(int curr, int k, vector<int>& prices){
+        if(curr == n || k == 0)
             return 0;
-        
+
         if(dp[curr][k] != -1)
             return dp[curr][k];
-        
+
         int mx = solve(curr + 1, k, prices);
         for(int i = curr + 1; i < n; i++){
-            if(prices[i] >= prices[curr]){
-                int profit = prices[i] - prices[curr] + solve(i + 1, k - 1, prices);
-                if(profit > mx)
-                    mx = profit;
-            }
-        }        
+            if(prices[i] > prices[curr])
+                mx = max(mx, prices[i] - prices[curr] + solve(i + 1, k-1, prices));
+        }
         return dp[curr][k] = mx;
     }
 };
