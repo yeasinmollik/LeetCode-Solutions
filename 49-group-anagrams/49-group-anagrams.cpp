4da7708b195ac5501ebdfd51803b3954c31ef3f0
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-09-16 20:43:20 +0600

    Time: 89 ms (23.18%), Space: 20.1 MB (68.99%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for(auto &s: strs){
-            string sorted = s;
+        int n = strs.size();
+        unordered_map<string, vector<int>> mp;
+        for(int i = 0; i < n; i++){
+            string sorted = strs[i];
             sort(sorted.begin(), sorted.end());
-            mp[sorted].push_back(s);
+            mp[sorted].push_back(i);
         }
         
         vector<vector<string>> ans;
         for(auto x: mp){
-            ans.push_back(x.second);
+            ans.push_back({});
+            for(int y: x.second)
+                ans.back().push_back(strs[y]);
         }
         return ans;
     }
