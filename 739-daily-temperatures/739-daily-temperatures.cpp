8564092feca5716dbbb7cf659ac6b16bede00212
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-09-24 06:31:10 +0600

    Time: 205 ms (82.14%), Space: 88.8 MB (51.20%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        stack<int> st;
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && temp[st.top()] < temp[i]){
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
