af061b49df22321513f0a077821bacace8f6241c
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-10-27 03:39:29 +0600

    Time: 82 ms (93.63%), Space: 45.5 MB (9.82%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cs
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public int FirstUniqChar(string s) {
-        int[] cnt = new int[26];
+        Dictionary<char, int> cnt = new Dictionary<char, int>();
         foreach(char c in s){
-            cnt[c - 'a']++;
+            if(cnt.ContainsKey(c))
+                cnt[c]++;
+            else
+                cnt[c] = 1;
         }
         for(int i = 0; i < s.Length; i++)
-            if(cnt[s[i] - 'a'] == 1)
+            if(cnt[s[i]] == 1)
                 return i;
         return -1;
     }
